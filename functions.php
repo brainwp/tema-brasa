5cf6f5a9a55bfc199793787c6d91b3b3c4e34db0
Diego Rojas <diego@brasa.art.br>
2014-01-10 12:52:03 -0200

    retirando as funções do tema que passarão a ser globais para toda a rede de sites da Brasa

--- functions.php
+++ functions.php
@@ -10,63 +10,11 @@ add_post_type_support( 'page', 'excerpt' );
 define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyten_header_image_width', 460 ) );
 define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyten_header_image_height', 290 ) );
 
-
-// Remove notificações de update do WP para usuários abaixo do Administrador
-global $user_login;
-get_currentuserinfo();
-if (!current_user_can('update_plugins')) { // checks to see if current user can update plugins
-add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
-add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
-}
-
-// Personaliza o rodapé do admin
-function custom_admin_footer() {
-        echo 'Desenvolvido com <a href=http://wordpress.org/>WordPress</a> por <a href=http://www.brasa.art.br>Brasa Design e Tecnologia</a>.';
-}
-add_filter('admin_footer_text', 'custom_admin_footer');
-
-// Remove o Item Editar do menu Aparência
-function remove_editor_menu() {
-  remove_action('admin_menu', '_add_themes_utility_last', 101);
-}
-
-add_action('_admin_menu', 'remove_editor_menu', 1);
-
 // Remove o MetaBox Format dos Posts
 add_action( 'admin_menu', 'remove_meta_boxes' );
 function remove_meta_boxes() {
     remove_meta_box( 'formatdiv', 'post', 'normal' ); // Post format meta box
 }
-
-// Custom login
-function my_custom_login_logo() {
-    echo '<style type="text/css">
-        h1 a { background:url('.get_bloginfo('stylesheet_directory').'/images/logo-medio.png) no-repeat scroll center top !important; 
-		height: 180px !important; width: 235px !important;  margin-top: -40px !important;  margin-left: 55px !important;
-		overflow: hidden;
-		}
-		body { background-image:url('.get_bloginfo('stylesheet_directory').'/images/bg-2000-1250.jpg) !important; }
-		#login { padding: 70px 0 0; }
-		
-    </style>';
-}
-
-add_action('login_head', 'my_custom_login_logo');
-
-// Remove Widgets do Wp-Admin
-function remove_dashboard_widgets() {
-	global $wp_meta_boxes;
-
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
-
-}
-
-add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
 
 //include new jQuery
 
@@ -75,26 +23,8 @@ wp_deregister_script( 'jquery' );
 wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
 wp_enqueue_script('jquery');
 wp_enqueue_script( 'mobile-nav', get_stylesheet_directory_uri() . '/js/mobile_nav.js', array('jquery'));
-	 
-
 }
 
 add_action('wp_enqueue_scripts', 'my_scripts_method');
-
-// FILTRO PARA TROCAR CERTAS PALAVRAS POR LINKS
-
-function replace_wordswithlinks($text){
-	$replace = array(
-	// 'WORD TO REPLACE' => 'REPLACE WORD WITH THIS'
-	'WordPress' => '<a href="http://brasa.art.br/sobre-wordpress/">WordPress</a>'
-	);
-	$text = str_replace(array_keys($replace), $replace, $text);
-	return $text;
-}
-
-add_filter('the_content', 'replace_wordswithlinks');
-add_filter('the_excerpt', 'replace_wordswithlinks');
-
-
 
 ?>
